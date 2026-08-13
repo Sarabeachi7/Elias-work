d588425de79aae03a421d25d25d84cbd75daf7ba
Elias Sarabeachi <eliassarabeachi@gmail.com>
2026-08-12 21:47:33 -0600

    Resuelto el problema

--- app.js
+++ app.js
@@ -629,3 +629,10 @@ app.delete("/curriculum/idiomas/:id", (req, res) => {
 // ===============================
 
 module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
+
+module.exports = app;
